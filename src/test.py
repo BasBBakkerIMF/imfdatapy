f38770dfb02c40d4faed9fd7ce336ccf950603d3
Allen Boddie <allen.boddie1@gmail.com>
2026-03-20 09:59:36 -0400

    remove testing script

--- src/test.py
+++
@@ -1,9 +0,0 @@
-from imfdatapy import IMFData
-
-connection = IMFData(False)
-#print(connection.datasets)
-print(connection.get_data('CPI', key = 'USA.CPI._T.IX.A'))
-#print(connection.listDataStructures('DSD_CPI'))
-#print('-'*10)
-#print('-'*10)
-#print(connection.getDataStructures('DSD_CPI'))
